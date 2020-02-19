a095fe5a7aff483605a280653c27853f41570d08
edwardchar <edwardchar@yahoo.com>
2020-02-18 20:21:49 -0500

    add widget service

--- src/main/java/com/example/demo/models/Widget.java
+++ src/main/java/com/example/demo/models/Widget.java
@@ -2,12 +2,85 @@ package com.example.demo.models;
 
 public class Widget {
 
-    private Integer id;
-
-    private String title = "New Widget";
     private String topicId;
+    private String name = "New Widget";
+    private String id;
     private String type = "HEADING";
+    private int order;
+    private String text = "Sample Text";
+    private String url = "https://www.example.com";
     private int size = 2;
+    private int width;
+    private int height;
+    private String cssClass;
+    private String style;
+    private String value;
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
 
     public int getSize() {
         return size;
@@ -33,25 +106,25 @@ public class Widget {
         this.topicId = topicId;
     }
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public String getName() {
+        return name;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Widget(Integer id, String title) {
+    public Widget(String id, String name) {
         this.id = id;
-        this.title = title;
+        this.name = name;
     }
 
     public Widget() {
